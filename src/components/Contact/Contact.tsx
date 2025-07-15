175af380080337eabf7765d4211f7170463a0b62
Ramprakash <77358753+i-am-ramprakash@users.noreply.github.com>
2025-07-15 22:55:02 +0530

    Added new files

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -32,7 +32,7 @@ const Contact: React.FC = () => {
       } else {
         setSubmitStatus('error');
       }
-    } catch (error) {
+    } catch {
       setSubmitStatus('error');
     } finally {
       setIsSubmitting(false);
